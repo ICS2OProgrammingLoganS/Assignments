7caad002426b84d0e1b44fce814bbe788fb35b63
Logan Sweeney <55555969+s278599@users.noreply.github.com>
2019-11-01 14:28:16 -0400

    updates

--- MathQuizLoganS/main.lua
+++ MathQuizLoganS/main.lua
@@ -1,7 +1,7 @@
 -- Title: NumericTextFields
--- Name: Your Name
--- Course: ICS2O/3C
--- This program displays a math question on the screen and asks the user to answer in a numeric textfield.
+-- Name: Logan Sweeney
+-- Course: ICS2O
+-- This program displays a math question on the screen and asks the user to answer in a numeric text field.
 
 --hide status bar
 display.setStatusBar (display.HiddenStatusBar)
@@ -23,20 +23,37 @@ local incorrectObject
 local points = 0
 local pointsText
 local randomOperator
+local totalSeconds = 15
+local secondsLeft = 15
+local clockText
+local countdownTimer
+local lives = 4
+local heart1
+local heart2
+local heart3
+local heart4
+local gameOverScreen
+local youWinScreen
 
-----------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------------------------
 --SOUNDS
-----------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
 
-audio.play (backgroundMusic, {loops = -1})
-local incorrectSound = audio.loadSound ("Sounds/Wrong Buzzer Sound Effect.mp3")
+local gameOverSound = audio.loadSound ("Game-over-yeah.mp3")
+local gameOverSoundChannel
+
+local incorrectSound = audio.loadSound ("Roblox-death-sound.mp3")
 local incorrectSoundChannel
-local correctSound = audio.loadSound ("Sounds/Correct Answer Sound Effect.mp3")
+
+local correctSound = audio.loadSound ("Correct Answer Sound Effect.mp3")
 local correctSoundChannel
 
-----------------------------------------------------------------------------
---FUNCTIONS
-----------------------------------------------------------------------------
+local YouWinSound = audio.loadSound ("youWin.mp3")
+
+--------------------------------------------------------------------------------------------------------------------
+--LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------------------------------
 
 local function AskQuestion()
 	randomOperator = math.random (1,4)
@@ -111,20 +128,85 @@ local function NumericFieldListener(event)
 		if (userAnswer == correctAnswer) then
 			-- give a point if user gets right answer.
 			points = points + 1
+
+			if (points == 5) then
+				youWinScreen.isVisible = true
+				numericField.isVisible = false
+			end
 			--update it in the display object
 			pointsText.text = "Points: " .. points
 			correctObject.isVisible = true
 			correctSoundChannel = audio.play (correctSound)
 			timer.performWithDelay (2000, HideCorrect)
+			secondsLeft = totalSeconds + 2
 		else
+			lives = lives - 1
+			secondsLeft = totalSeconds + 2
 			incorrectObject.text = "You got it wrong! The correct answer was " .. correctAnswer .." !"
-			incorrectSoundChannel = audio.play (incorrectSound)
 			incorrectObject.isVisible = true
 			timer.performWithDelay (2000, HideIncorrect)
+			if (lives == 3) then
+
+				heart4.isVisible = false
+				incorrectSoundChannel = audio.play (incorrectSound)
+			elseif (lives == 2) then
+				heart3.isVisible = false
+				incorrectSoundChannel = audio.play (incorrectSound)
+			elseif (lives == 1) then
+				heart2.isVisible = false
+				incorrectSoundChannel = audio.play (incorrectSound)
+			elseif (lives  == 0) then
+				heart1.isVisible = false
+				gameOverScreen.isVisible = true
+				numericField.isVisible = false
+				gameOverSoundChannel = audio.play (gameOverSound)
+				clockText.isVisible = false
+			end
 		end
 	end
 end
 
+local function UpdateTime()
+	secondsLeft = secondsLeft - 1
+
+	--display the number of seconds left in the clock object
+	clockText.text = "Time Left: " .. secondsLeft .. ""
+
+	if (secondsLeft == 0) then
+		--reset the number of seconds left
+		secondsLeft = totalSeconds
+		lives = lives - 1
+
+		-- *** IF THERE ARE NO LIVES LEFT, PLAY A LOSE SOUND SHOW A YOU LOSE IMAGE
+		--AND CANCEL THE TIMER REMOVE THE THIRD FOURTH HEART BY MAKING IT INVISIBLE
+		if (lives == 3) then
+			heart4.isVisible = false
+			incorrectSoundChannel = audio.play (incorrectSound)
+		elseif (lives == 2) then
+			heart3.isVisible = false
+			incorrectSoundChannel = audio.play (incorrectSound)
+		elseif (lives == 1) then
+			heart2.isvisible = false
+			incorrectSoundChannel = audio.play (incorrectSound)
+		elseif (lives  == 0) then
+			gameOverSoundChannel = audio.play (gameOverSound)			
+			heart4.isVisible = false
+			gameOverScreen.isVisible = true
+			numericField.isVisible = false
+			clockText.isVisible = false
+		end
+	
+		-- *** CALL THE FUNCTION TO ASK A NEW QUESTION
+		AskQuestion()
+    end 
+end
+
+-- function that calls the timer
+local function StartTimer()
+	--create a countdown timer that loops infinitely
+	countdownTimer = timer.performWithDelay (1000, UpdateTime, 0)
+end
+StartTimer()
 ----------------------------------------------------------------------------
 --OBJECT CREATION
 ----------------------------------------------------------------------------
@@ -152,6 +234,35 @@ incorrectObject.isVisible = false
 
 -- display the amount of points as a text object
 pointsText = display.newText ("Points: " .. points, display.contentWidth/3, display.contentHeight/3, nil, 50)
+
+heart1 = display.newImageRect ("rainbow-heart.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect ("rainbow-heart.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect ("rainbow-heart.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect ("rainbow-heart.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+
+gameOverScreen = display.newImageRect ("gameOver.jfif", 1536, 768)
+gameOverScreen.x = display.contentWidth/2
+gameOverScreen.y = display.contentHeight/2
+gameOverScreen.isVisible = false
+
+clockText = display.newText ("Time Left: " .. secondsLeft, display.contentWidth/7, display.contentHeight/7, nil, 50)
+
+youWinScreen = display.newImageRect ("youWin.jpg", 1536, 768)
+youWinScreen.x = display.contentWidth/2
+youWinScreen.y = display.contentHeight/2
+youWinScreen.isVisible = false
 
 ----------------------------------------------------------------------------------------------------------------------
 --FUNCTION CALLS
